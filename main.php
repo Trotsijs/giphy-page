8d9e4c3572101edc61e1f7dd0aeddeac7d3a1809
Trotsijs <trotsijs@gmail.com>
2023-04-23 12:34:19 +0300

    Updated

--- main.php
+++ main.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>GIPHY Page</title>
-    <link rel="stylesheet" href="./style.css">
-    <link rel="icon" href="./favicon.ico" type="image/x-icon">
-  </head>
-  <body>
-    <main>
-        <h1>Hello</h1>
-    </main>
-  </body>
-</html>
+<?php declare(strict_types=1);
+
+echo '<h1>Hello World</h1>';
